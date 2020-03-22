0654e8787ab053ca21977d1ec270182608c7229a
lixinyang <xt623270307@outlook.com>
2020-03-23 00:06:29 +0800

    配置地址

--- my-blog/dpl.sh
+++ my-blog/dpl.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f https://gitee.com/taotao521/blog.git master:gh-pages
+git push -f https://github.com/18522640223/blog.git master:gh-pages
 
 cd -
